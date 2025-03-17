3a6f2a28aad039daaf71b5ae80598c57c4db9d44
khemisse-radwane <khemisse.radwane@gmail.com>
2025-03-17 14:15:29 +0000

    instanciation of patient using constructor with and with no parameters + using builder

--- hopital/src/main/java/ma/enset/hopital/HopitalApplication.java
+++ hopital/src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -1,13 +1,44 @@
 package ma.enset.hopital;
 
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HopitalApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class HopitalApplication implements CommandLineRunner {
+
+	@Autowired
+	private PatientRepository patientRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(HopitalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// En utilisant le constructeur par defaut
+		Patient patient = new Patient();
+		patient.setId(null);
+		patient.setName("redone");
+		patient.setDateOfBirth(new Date());
+		patient.setSick(true);
+		patient.setScore(10);
+
+		// En utilisant le constructeur
+		Patient patient2 = new Patient(null, "Hassan", new Date(), true, 100);
+
+		// En utilisons builder
+		Patient patient3 = Patient.builder()
+				.name("karima")
+				.dateOfBirth(new Date())
+				.score(53)
+				.isSick(true)
+				.build();
+
+
+	}
 }
